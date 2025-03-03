f4d7a0f629c824bd3b93ed35d966b3a5864f6013
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2025-03-03 23:09:35 +1030

    updated home page

--- FizzBuzz/frontend/src/App.tsx
+++ FizzBuzz/frontend/src/App.tsx
@@ -1,17 +1,19 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import CreateGamePage from './pages/CreateGamePage';
 import PlayGamePage from './pages/PlayGamePage';
+import HomePage from './pages/HomePage';
 
 function App() {
     return (
         <div>
-            <nav>
-                <Link to="/">Play Game</Link> | {/* Link to the default page */}
-                <Link to="/create-game">Create Game</Link>
-            </nav>
+            {/*<nav>*/}
+            {/*    <Link to="/">Play Game</Link> | */}{/* Link to the default page */}
+            {/*    <Link to="/create-game">Create Game</Link>*/}
+            {/*</nav>*/}
 
             <Routes>              
-                <Route path="/" element={<PlayGamePage />} />
+                <Route path="/" element={<HomePage />} />
+
                 <Route path="/create-game" element={<CreateGamePage />} />
                 <Route path="/play/:sessionId" element={<PlayGamePage />} />
             </Routes>
---
+++ FizzBuzz/frontend/src/pages/HomePage.tsx
@@ -0,0 +1,44 @@
+﻿import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../CSS/HomePage.css'; // We'll define CSS in a separate file
+
+function HomePage() {
+    const navigate = useNavigate();
+
+    // Handlers to navigate to other pages
+    const handleCreateGame = () => {
+        navigate('/create-game'); // or whichever route you use
+    };
+
+    const handlePlayGame = () => {
+        navigate('/play'); // or whichever route you use
+    };
+
+    return (
+        <div className="home-container">
+            <h1 className="home-title">FIZZ BUZZER</h1>
+
+            <p className="home-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                In this game, you’ll replace numbers with words like "Foo" or "Boo"
+                based on divisibility rules. Combine words if multiple rules apply!
+            </p>
+
+            <div className="boxes-container">
+                <div className="box box-foo">Foo</div>
+                <div className="box box-boo">Boo</div>
+            </div>
+
+            <div className="buttons-container">
+                <button className="nav-button" onClick={handleCreateGame}>
+                    Create Game
+                </button>
+                <button className="nav-button" onClick={handlePlayGame}>
+                    Play Game
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
